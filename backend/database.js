ec7210ab8d9c5a0423a9658ca30cff3bc7295983
Antti <antti.niiranen963@hotmail.fi>
2024-03-25 10:43:44 +0200

    sql user teksti

--- backend/database.js
+++ backend/database.js
@@ -2,7 +2,7 @@ const mysql=require('mysql2');
 const dotenv=require('dotenv');
 dotenv.config();
 
-const conn="mysql://uniuser:unipass@127.0.0.1:3306/university";
+const conn="mysql://user@localhost@127.0.0.1:3306/bank_automat";
 
 const connection=mysql.createPool(conn);
 
