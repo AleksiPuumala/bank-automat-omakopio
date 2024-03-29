bab6aa6ed56fb1e59d1b624fc785766f74f12855
Antti <antti.niiranen963@hotmail.fi>
2024-03-29 15:21:43 +0200

    card operaatiot ja pin cryptaus

--- backend/models/card_model.js
+++ backend/models/card_model.js
@@ -1,6 +1,5 @@
-//card taulujen operaatiot
-
 const db=require('../database');
+const bcrypt=require('bcryptjs');
 
 const card={
     getAllCard(callback){
@@ -10,10 +9,14 @@ const card={
         return db.query('SELECT * FROM card WHERE cardnumber=?',id,callback);
     },
     addCard(newCard,callback){
-        return db.query('INSERT INTO card(cardpin,cardtype,cardnumber) VALUES(?,?,?)',[newCard.cardpin,newCard.cardtype,newCard.cardnumber],callback);
+        bcrypt.hash(newCard.cardpin,10,function(err,hashPin){
+            return db.query('INSERT INTO card(cardpin,cardtype,cardnumber) VALUES(?,?,?)',[hashPin,newCard.cardtype,newCard.cardnumber],callback);
+        });
     },
     updateCard(id,updatecard,callback){
-        return db.query('UPDATE card SET cardpin=?, cardtype=? WHERE cardnumber=?',[updatecard.cardpin,updatecard.cardtype,id],callback);
+        bcrypt.hash(updatecard.cardpin,10,function(err,hashPin){
+            return db.query('UPDATE card SET cardpin=?, cardtype=? WHERE cardnumber=?',[hashPin,updatecard.cardtype,id],callback)
+        });
     },
     deleteCard(id,callback){
         return db.query('DELETE FROM card WHERE cardnumber=?',[id],callback);
@@ -23,4 +26,6 @@ const card={
 }
 
 module.exports=card;
+
+
 
