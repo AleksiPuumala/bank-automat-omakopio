7155e91aadfdde750b33b7f246921a7bda505192
AleksiPuumala <aleksi.puumala@gmail.com>
2024-04-02 10:54:36 +0300

    Update transaction_model.js

--- backend/models/transaction_model.js
+++ backend/models/transaction_model.js
@@ -5,7 +5,7 @@ const transaction={
         return db.query("SELECT * FROM transaction",callback);
     },
     addTransaction(newTransaction, callback){
-        return db.query("INSERT INTO transaction VALUES(?,?,?,?,?,?)",[newTransaction.idaccount,newTransaction.time,newTransaction.transactionamount, newTransaction.transactiontype,newTransaction.idtransaction,newTransaction.cardnumber],callback);
+        return db.query("INSERT INTO transaction VALUES(?,?,?,?,?)",[newTransaction.idaccount,newTransaction.time,newTransaction.transactionamount, newTransaction.transactiontype,newTransaction.cardnumber],callback);
     },
     updateTransaction(updatedTransaction, callback){
         return db.query("UPDATE transaction SET time=?, transactionamount=?, transactiontype=?, cardnumber=? WHERE idtransaction=?", [time, transactionamount, transactiontype, cardnumber, idaccount, idtransaction], callback);
