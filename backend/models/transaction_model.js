66c193677a2ba5cb4eacfe107a6ccbbd096e4d2c
AleksiPuumala <aleksi.puumala@gmail.com>
2024-04-04 14:25:40 +0300

    Update transaction_model.js

--- backend/models/transaction_model.js
+++ backend/models/transaction_model.js
@@ -5,16 +5,16 @@ const transaction={
         return db.query("SELECT * FROM transaction",callback);
     },
     addTransaction(newTransaction, callback){
-        return db.query("INSERT INTO transaction VALUES(?,?,?,?,?)",[newTransaction.idaccount,newTransaction.time,newTransaction.transactionamount, newTransaction.transactiontype,newTransaction.cardnumber],callback);
+        return db.query("INSERT INTO transaction (idaccount, time, transactionamount, transactiontype, cardnumber) VALUES(?,?,?,?,?)",[newTransaction.idaccount,newTransaction.time,newTransaction.transactionamount, newTransaction.transactiontype,newTransaction.cardnumber],callback);
     },
-    updateTransaction(updatedTransaction, callback){
-        return db.query("UPDATE transaction SET time=?, transactionamount=?, transactiontype=?, cardnumber=? WHERE idtransaction=?", [time, transactionamount, transactiontype, cardnumber, idaccount, idtransaction], callback);
+    updateTransaction(idtransaction, updatedTransaction, callback){
+        return db.query("UPDATE transaction SET time=?, transactionamount=?, transactiontype=?, cardnumber=? WHERE idtransaction=?", [updatedTransaction.time, updatedTransaction.transactionamount, updatedTransaction.transactiontype, updatedTransaction.cardnumber, idtransaction], callback);
     },
     getOneTransaction(idtransaction, callback) {
-        return db.query("SELECT * FROM transaction WHERE idtransaction=?", [idtransaction], callback);
+        return db.query("SELECT * FROM transaction WHERE idtransaction=?", idtransaction, callback);
     },
     deleteTransaction(idtransaction, callback) {
-        return db.query("DELETE FROM transaction WHERE idtransaction=?", [idtransaction], callback);
+        return db.query("DELETE FROM transaction WHERE idtransaction=?", idtransaction, callback);
     }
 }
 
