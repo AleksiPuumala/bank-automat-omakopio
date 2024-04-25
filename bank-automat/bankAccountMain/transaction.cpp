12e905295819692a2ebf4562c99fcae8478ab35a
Antti <antti.niiranen963@hotmail.fi>
2024-04-25 19:21:19 +0300

    tilitapahtumien selaus ok

--- bank-automat/bankAccountMain/transaction.cpp
+++ bank-automat/bankAccountMain/transaction.cpp
@@ -37,31 +37,81 @@ void transaction::showTransactionSlot(QNetworkReply *reply)
     response_data=reply->readAll();
     tArray = QJsonDocument::fromJson(response_data).array();
 
-    QStandardItemModel *table_model = new QStandardItemModel(tList.size(),4);
+    table_model = new QStandardItemModel(tList.size(),5);
     table_model->setHeaderData(0,Qt::Horizontal,QObject::tr("Korttinumero"));
     table_model->setHeaderData(1,Qt::Horizontal,QObject::tr("Tapahtuma"));
     table_model->setHeaderData(2,Qt::Horizontal,QObject::tr("Määrä"));
     table_model->setHeaderData(3,Qt::Horizontal,QObject::tr("Aika"));
+    table_model->setHeaderData(4,Qt::Horizontal,QObject::tr("Tapahtuma ID"));
+
 
     for (int row=0;row < tArray.size(); ++row) {
+        qDebug()<<row;
         QJsonObject jsObj=tArray.at(row).toObject();
         cnum = jsObj["cardnumber"].toString();
         timestamp = jsObj["time"].toString();
         amount = jsObj["transactionamount"].toString();
         ttype = jsObj["transactiontype"].toString();
+        tid = QString::number(jsObj["idtransaction"].toInt());
 
-        QStandardItem *cardnumitem = new QStandardItem(cnum);
+        cardnumitem = new QStandardItem(cnum);
         table_model->setItem(row, 0, cardnumitem);
-        QStandardItem *timestampitem = new QStandardItem(timestamp);
+        timestampitem = new QStandardItem(timestamp);
         table_model->setItem(row, 3, timestampitem);
-        QStandardItem *amountitem = new QStandardItem(amount);
+        amountitem = new QStandardItem(amount);
         table_model->setItem(row, 2, amountitem);
-        QStandardItem *typeitem = new QStandardItem(ttype);
+        typeitem = new QStandardItem(ttype);
         table_model->setItem(row, 1, typeitem);
+        tranid = new QStandardItem(tid);
+        table_model->setItem(row,4,tranid);
+
     }
-    ui->table_model->setModel(table_model);
+    ui->tableview->setModel(table_model);
+    for(int row=0;row<tArray.size();row++){
+    ui->tableview->hideRow(row);
+    }
+    rowStart=0;
+    rowEnd=5;
+    for(int row=rowStart;row<rowEnd;row++){
+        ui->tableview->showRow(row);
+    }
 }
 
+void transaction::on_btnNext_clicked()
+{
+    nextTable();
+}
 
+void transaction::on_btnBack_clicked()
+{
+    backTable();
+}
+
+void transaction::nextTable()
+{
+    for(int row=0;row<tArray.size();row++){
+        ui->tableview->hideRow(row);
+    }
+    int x=rowStart;
+    rowStart=x+5;
+    x=rowEnd;
+    rowEnd=x+5;
+    for(int row=rowStart;row<rowEnd;row++){
+        ui->tableview->showRow(row);
+    }
+}
+
+void transaction::backTable()
+{
+    qDebug()<<rowStart+rowEnd;
+    for(int row=0;row<tArray.size();row++){
+        ui->tableview->hideRow(row);
+    }
+    rowStart=rowStart-5;
+    rowEnd=rowEnd-5;
+    for(int row=rowStart;row<rowEnd;row++){
+        ui->tableview->showRow(row);
+    }
+}
 
 
--- bank-automat/bankAccountMain/transaction.h
+++ bank-automat/bankAccountMain/transaction.h
@@ -25,15 +25,25 @@ private slots:
     void transactionSlot(QString, QByteArray);
     void showTransactionSlot(QNetworkReply*);
 
+    void on_btnNext_clicked();
+    void on_btnBack_clicked();
 
 private:
     Ui::transaction *ui;
     QNetworkReply *reply;
     QByteArray response_data;
     QNetworkAccessManager *transactionManager;
-    QString cnum, timestamp, amount, ttype;
-    QList<QString> tList {cnum, timestamp, amount, ttype};
+    QString cnum, timestamp, amount, ttype, tid;
+    //QList<QString> tList {cnum, timestamp, amount, ttype, tid};
+    QList<QStandardItem*> tList {typeitem, amountitem,timestampitem, cardnumitem, tranid};
     QJsonArray tArray;
+    QStandardItemModel *table_model;
+    QStandardItem *typeitem, *amountitem, *timestampitem, *cardnumitem, *tranid;
+
+    int rowcount, rowStart, rowEnd;
+    void makeTable(int,int);
+    void nextTable();
+    void backTable();
 
 
 signals:
